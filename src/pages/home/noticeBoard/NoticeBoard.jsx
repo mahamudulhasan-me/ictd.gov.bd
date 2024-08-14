b8f8371deab659ad2937359dc5a4ee4376dabadc
mahamudulhasan-me <mahamudulhasan.org@gmail.com>
2024-08-14 19:19:10 +0600

    notich board complete

--- src/pages/home/noticeBoard/NoticeBoard.jsx
+++ src/pages/home/noticeBoard/NoticeBoard.jsx
@@ -1,6 +1,7 @@
 "use client";
 import AccordionItem from "@/components/ui/AccordionItem";
 import { notices } from "@/utils/notices";
+import Image from "next/image";
 import { useState } from "react";
 import { MdKeyboardDoubleArrowRight } from "react-icons/md";
 
@@ -12,14 +13,17 @@ export default function NoticeBoard() {
   };
 
   return (
-    <div
-      className="relative font-inter antialiased col-span-9 my-10 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]
-p-4 rounded-md"
-    >
-      <main className="relative flex flex-col justify-center overflow-hidden">
+    <div className="relative font-inter antialiased col-span-9 my-10 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] p-4 rounded-md bg-gradient-to-tr from-gray-200/40 to-gray-50/40">
+      <main className="relative flex flex-col justify-center overflow-hidden ">
         <div>
-          <h1 className="text-2xl font-bold text-slate-900 mb-4">
-            নোটিশ বোর্ড
+          <h1 className="text-2xl font-bold text-slate-900 mb-4 flex items-center gap-2">
+            <Image
+              src={"/images/icons/pin.png"}
+              width={24}
+              height={24}
+              alt="pin"
+            />
+            নোটিশ বোর্ড{" "}
           </h1>
           {/* Accordion component */}
           <div className="divide-y divide-gray-300">
@@ -39,6 +43,13 @@ p-4 rounded-md"
           </button>
         </div>
       </main>
+      <Image
+        src={"/images/Japan_map.png"}
+        width={300}
+        height={300}
+        alt="world map"
+        className="absolute top-2 right-2 -z-10 opacity-20 rotate-slow"
+      />
     </div>
   );
 }
